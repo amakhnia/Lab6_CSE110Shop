7ac4da1d49b65b7ff44fbefb6004b3786defa5f4
amakhnia <57231301+amakhnia@users.noreply.github.com>
2021-02-13 10:53:58 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,23 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  //initializing objects and fetching from given api
+  if(localstorage.getItem('obj') == NULL){
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => localstorage.setItem('obj', JSON.stringify(data)) //convert between JSON object to string
+    .then(addtoCart(cart));
+  }
+  //initializing cart
+  if(localstorage.getItem('cart') == NULL){
+    localstorage.setItem('cart', JSON.stringify(cart))
+  }
+  else{
+    addtoCart()
+  }
 });
+  
+//adding to cart and counting  
+function addtoCart(){
+  
+}
