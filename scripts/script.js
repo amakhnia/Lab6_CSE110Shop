9214b7f95bb3e76968979a061cfbd6ddf9ab1dd4
Nick Krolikowski <nkroliko@ucsd.edu>
2021-02-13 18:53:57 -0800

    null lowercase

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,14 @@
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
   //initializing objects and fetching from given api
-  if(localStorage.getItem('obj') == NULL){
+  if(localStorage.getItem('obj') == null){
     fetch('https://fakestoreapi.com/products')
     .then(response => response.json())
     .then(data => localStorage.setItem('obj', JSON.stringify(data))) //convert between JSON object to string
     .then(addtoCart(cart));
   }
   //initializing cart
-  if(localStorage.getItem('cart') == NULL){
+  if(localStorage.getItem('cart') == null){
     localStorage.setItem('cart', JSON.stringify(cart))
   }
   else{
